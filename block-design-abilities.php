ef0cbee4fd3bf9340d6d86285d72de4fae2ee519
Kadim Gültekin <35685546+Arkenon@users.noreply.github.com>
2026-05-04 11:19:10 +0300

    Update PHP version requirements in block-design-abilities.php

--- block-design-abilities.php
+++ block-design-abilities.php
@@ -12,8 +12,8 @@
  * License:           GPL v2 or later
  * License URI:       https://www.opensource.org/licenses/GPL-2.0
  * GitHub Plugin URI: https://github.com/Arkenon/block-design-abilities
- * Requires at least: 7.0
- * Requires PHP:      8.0
+ * Requires at least: 6.9
+ * Requires PHP:      7.4
  */
 
 
